21234493f46801260a7ba103fbdb5657c7453623
SmokingSpy <Olivergreen104@gmail.com>
2024-12-05 13:04:32 -0500

    backend tweak

--- sentimentDB_backend.py
+++ sentimentDB_backend.py
@@ -8,21 +8,9 @@ def main():
     reviews_csv = st.file_uploader("Upload Reviews CSV", type="csv")
 
     if reviews_csv is not None:
-        # To read file as bytes:
-        bytes_data = reviews_csv.getvalue()
-        st.write(bytes_data)
-
-        # To convert to a string based IO:
-        stringio = StringIO(reviews_csv.getvalue().decode("utf-8"))
-        st.write(stringio)
-
-        # To read file as string:
-        string_data = stringio.read()
-        st.write(string_data)
-
         # Can be used wherever a "file-like" object is accepted:
-        dataframe = pd.read_csv(uploaded_file)
-        st.write(dataframe)
+        df = pd.read_csv(reviews_csv)
+        st.write(df)
 
 if __name__ == "__main__":
     main()
